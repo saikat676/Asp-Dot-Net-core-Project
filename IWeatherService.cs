f1c82739b75bc6faed8971f01eba5f5dd02458e5
saikat676 <57571284+saikat676@users.noreply.github.com>
2025-07-17 20:25:02 +0530

    Add files via upload

---
+++ IWeatherService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ModelProject2;
+namespace WeatherServiceInterface
+{
+    public interface IWeatherService
+    {
+        List<CityWeather> GetWeatherDetails();
+        CityWeather? GetWeatherByCityCode(string CityCode);
+    }
+}
